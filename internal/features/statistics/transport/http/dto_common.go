d2f72da9e0c25ddaefa4f30e3cd9917eaa4ac128
Mark Popov <hawk691@gmail.com>
2026-05-09 12:02:24 +0300

    Init feature statistics implemented with full functionality

---
+++ internal/features/statistics/transport/http/dto_common.go
@@ -0,0 +1,24 @@
+package statistics_transport_http
+
+import "github.com/PopovMarko/todo_app/internal/core/domain"
+
+type SatatisticsDTOResponse struct {
+	TotalTasks                int      `json:"total_tasks"`
+	CompletedTasks            int      `json:"completed_tasks"`
+	TasksCompletionRate       *float64 `json:"tasks_completion_rate"`
+	TasksAverageCompetionTime *string  `json:"tasks_average_completion_time"`
+}
+
+func dtoStatisticsFromDomain(domainStatistics domain.Statistics) SatatisticsDTOResponse {
+	var tasksAverageCompletionString *string
+	if domainStatistics.TasksAverageCompletionTime != nil {
+		duration := domainStatistics.TasksAverageCompletionTime.String()
+		tasksAverageCompletionString = &duration
+	}
+	return SatatisticsDTOResponse{
+		TotalTasks:                domainStatistics.TotalTasks,
+		CompletedTasks:            domainStatistics.CompletedTasks,
+		TasksCompletionRate:       domainStatistics.TasksCompletionRate,
+		TasksAverageCompetionTime: tasksAverageCompletionString,
+	}
+}
